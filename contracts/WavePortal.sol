ce8cf0364c9d2986452ee6d3de57b43e940d9e70
Stefanie <stefcodes@gmail.com>
2021-11-08 00:49:42 -0800

    add count to contract and changed to messages

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,18 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalMessages; 
     constructor() {
         console.log("This contract is smart and gmi");
+    }
+
+    function message() public {
+        totalMessages += 1; 
+        console.log("% has messaged you!", msg.sender); 
+    }
+
+    function getTotalMessages() public view returns (uint256) {
+        console.log("We have %d total messages!", totalMessages); 
+        return totalMessages; 
     }
 }
