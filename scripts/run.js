a343a9e57a1e765f1a4854bb6307549611934887
Stefanie <stefcodes@gmail.com>
2021-11-08 00:50:07 -0800

    update script to match contract

--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,19 @@
 const main = async () => {
+    const [owner,randomPerson] = await hre.ethers.getSigners(); 
     const waveContractFactory = await hre.ethers.getContractFactory('WavePortal'); 
     const waveContract = await waveContractFactory.deploy(); 
     await waveContract.deployed(); 
+
     console.log("Contract deployed to:", waveContract.address);
+    console.log("Contract deployed by:", owner.address); 
+
+    let messageCount; 
+    messageCount = await waveContract.getTotalMessages(); 
+
+    const msgTxn = await waveContract.message(); 
+    await msgTxn.wait(); 
+
+    messageCount = await waveContract.getTotalMessages(); 
 };
 
 const runMain = async () => {
