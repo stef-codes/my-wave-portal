95bc4eccac1f47602dce667beb903cb38ff7f849
Stefanie <stefcodes@gmail.com>
2021-11-07 23:29:06 -0800

    fix syntax

--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,8 @@
 const main = async () => {
-    const waveContractFactory = await.hre.ethers.getContractFactory('WavePortal'); 
+    const waveContractFactory = await hre.ethers.getContractFactory('WavePortal'); 
     const waveContract = await waveContractFactory.deploy(); 
     await waveContract.deployed(); 
-    console.log("Contract deployed to":, waveContract.address);
+    console.log("Contract deployed to:", waveContract.address);
 };
 
 const runMain = async () => {
